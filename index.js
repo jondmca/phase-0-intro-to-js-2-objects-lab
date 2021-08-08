2f8f5af80d1b25fe1d1780ca88eaf8201f7bc6fa
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-08-08 16:40:46 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Derek",
+    streetAddress: "11742 W Bellfort St.",
+};
+function updateEmployeeWithKeyAndValue(employee){
+    const newEmployee = {...employee};
+    newEmployee.name = "Sam";
+    newEmployee.streetAddress = "11 Broadway";
+    return newEmployee;
+    }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey(employee){
+    const newEmployeeByKey = {...employee};
+    delete newEmployeeByKey.name;
+    return newEmployeeByKey;
+}
+function destructivelyDeleteFromEmployeeByKey(employee){
+    delete employee.name;
+    return employee;
+}
